e9545ccd5f00b446ded751f3e5b27ed4787cc49c
gisergey <Ginsburg98@inbox.ru>
2021-12-11 00:46:40 +0300

    ошибка исправлена

--- Hometask_13/Tasks.cs
+++ Hometask_13/Tasks.cs
@@ -9,7 +9,7 @@ namespace Hometask_13
     class Tasks
     {
         private string[] tasks = { "задание 1", "задание 2", "задание 3", "задание 4", "задание 5", "задание 6", "задание 7", "задание 8", "задание 9", "задание 10" };
-        private string this[int index]
+        public string this[int index]
         {
             get { return tasks[index]; }
         }
